67d66b74888ce2b9d6eac612d9f0308d66871dcf
Joshua Birchfield <joshua.birchfield32@gmail.com>
2023-01-19 19:21:37 -0600

    Add project files.

---
+++ gitPractice.cpp
@@ -0,0 +1,11 @@
+//Name: Joshua Birchfield
+//Description: This program is to show that I know how to upload to github
+//Date: January 19, 2023
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "This is practice for the github assignment." << endl;
+}
